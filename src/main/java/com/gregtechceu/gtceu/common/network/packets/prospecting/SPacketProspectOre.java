78e624e9ca1b5438553a2de3f9fb79e1211d8315
omergunr100 <omer.er10@gmail.com>
2024-12-30 18:51:18 +0200

    finalized new ore prospecting packet

--- src/main/java/com/gregtechceu/gtceu/common/network/packets/prospecting/SPacketProspectOre.java
+++ src/main/java/com/gregtechceu/gtceu/common/network/packets/prospecting/SPacketProspectOre.java
@@ -6,8 +6,16 @@ import com.gregtechceu.gtceu.integration.map.cache.client.GTClientCache;
 import com.lowdragmc.lowdraglib.networking.IHandlerContext;
 
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
+
+import java.util.Collection;
 
 public class SPacketProspectOre extends SPacketProspect<GeneratedVeinMetadata> {
+
+    public SPacketProspectOre(ResourceKey<Level> key, Collection<GeneratedVeinMetadata> veins) {
+        super(key, veins.stream().map(GeneratedVeinMetadata::center).toList(), veins);
+    }
 
     @Override
     public void encodeData(FriendlyByteBuf buf, GeneratedVeinMetadata data) {
